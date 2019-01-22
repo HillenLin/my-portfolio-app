a9db7989a0cb92e0d40bf0c361c9a7256371f6bf
Kevin Lin <Kevin.Lin@dickerdata.com.au>
2019-01-22 17:12:59 +1100

    Adding header and sidebar

--- src/app/my-nav/my-nav.module.ts
+++ src/app/my-nav/my-nav.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [SidebarComponent],
-  exports: [SidebarComponent],
+  declarations: [SidebarComponent, HeaderComponent],
+  exports: [SidebarComponent, HeaderComponent],
   providers: []
 })
 export class MyNavModule { }
